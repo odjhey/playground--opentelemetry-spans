dc7d612875f9ea2ef98befd5fd33dae1dc65f4d2
Odelon Jhey Pacalso <odjhey@gmail.com>
2024-05-22 10:56:14 +0900

    example child span

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,34 @@
+import { trace } from '@opentelemetry/api'
+import { api } from '@opentelemetry/sdk-node'
 import fastify from 'fastify'
 
 const server = fastify()
 
+const tracer = trace.getTracer('custom-tracer-19387')
+
 server.get('/ping', async (request, reply) => {
-  return 'pong\n'
+  const span = api.trace.getSpan(api.context.active())
+  span?.setAttribute('custom-attribute-asdfl123', 'custom-value-9o8uc')
+  console.log(span)
+
+  if (span) {
+    await new Promise((res) => {
+      const childSpan = tracer.startSpan(
+        'new-span',
+        undefined,
+        api.context.active()
+      )
+
+      setTimeout(() => {
+        childSpan.setAttribute('kuku2', 'lala')
+        childSpan.end()
+        res({})
+      }, 1000)
+    })
+    return 'pongers'
+  } else {
+    return 'pong\n'
+  }
 })
 
 server.listen({ port: 8080 }, (err, address) => {
